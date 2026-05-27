c98461fe0c4a7ff2546df16de605d01118be613c
Antonio Gomez Hernandez <antoniomgh@gmail.com>
2026-05-28 01:05:58 +0200

    WIP 02

--- gh-sp-simplesamlphp/var-simplesamlphp/metadata/saml20-idp-hosted.php
+++ gh-sp-simplesamlphp/var-simplesamlphp/metadata/saml20-idp-hosted.php
@@ -14,8 +14,6 @@ $metadata['https://secaas-labs-poc-01.org/idp/simplesamlphp'] = array(
 	'host' => 'idptestbed.localhost',
 
 	// X.509 key and certificate. Relative to the cert directory.
-	# TODO 'privatekey' => '/run/secrets/ssp_key',
-	# TODO 'certificate' => 'server.crt',
     'certificate' => '/run/secrets/sp_simple_cert',
     'privatekey' => '/run/secrets/sp_simple_key',
 	/*
